d769351045a647dc8e5e84af60823296284d25d7
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-03 10:42:58 +0100

    add BankAccount interface

--- src/main/java/e1/BankAccount.java
+++ src/main/java/e1/BankAccount.java
@@ -1,21 +1,7 @@
 package e1;
 
-public class BankAccount {
-
-    private CoreBankAccount base = new CoreBankAccount();
-
-    public int getBalance() {
-        return base.getBalance();
-    }
-
-    public void deposit(int amount) {
-        base.deposit(amount);
-    }
-
-    public void withdraw(int amount) {
-        if (this.getBalance() < amount){
-            throw new IllegalStateException();
-        }
-        base.withdraw(amount + 1);
-    }
+public interface BankAccount {
+    public int getBalance();
+    public void deposit(int amount);
+    public void withdraw(int amount);
 }
