e06c12b276694a3751e708b90ce5cdc8c37595c7
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-03 11:15:02 +0100

    add javadoc to BankAccount

--- src/main/java/e1/BankAccount.java
+++ src/main/java/e1/BankAccount.java
@@ -1,7 +1,24 @@
 package e1;
 
+/**
+ * Models a Bank Account
+ */
 public interface BankAccount {
+    /**
+     * Return the current balance.
+     * @return the balance.
+     */
     public int getBalance();
+
+    /**
+     * Adds the amount to the balance.
+     * @param amount amount to be added to the balance.
+     */
     public void deposit(int amount);
+
+    /**
+     * Removes the amount from th balance.
+     * @param amount amount to be removed from the balance.
+     */
     public void withdraw(int amount);
 }
