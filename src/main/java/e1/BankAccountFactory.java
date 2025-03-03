b073051a088b2aa3caedb4e579e2042aa087d132
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-03 14:21:47 +0100

    adapt bronze account to use FeeDecorator

--- src/main/java/e1/BankAccountFactory.java
+++ src/main/java/e1/BankAccountFactory.java
@@ -4,6 +4,7 @@ public class BankAccountFactory {
 
     public static final int DEFAULT_OVERDRAFT_AMOUNT_LIMIT = 0;
     public static final int FEE = 1;
+    public static final int FREE_WITHDRAW_MAX_AMOUNT = 100;
 
     public BankAccount getSilverBankAccount() {
         return new BankAccountDecorator(
@@ -23,7 +24,13 @@ public class BankAccountFactory {
 
     public BankAccount getBronzeBankAccount() {
         return new BankAccountDecorator(
-                new OverdraftDecorator(new BronzeBankAccount(new CoreBankAccount()), DEFAULT_OVERDRAFT_AMOUNT_LIMIT)
+                new OverdraftDecorator(
+                        new FeeDecorator(
+                                new CoreBankAccount(),
+                                amount -> amount < FREE_WITHDRAW_MAX_AMOUNT ? 0 : FEE
+                        ),
+                        DEFAULT_OVERDRAFT_AMOUNT_LIMIT
+                )
         );
     }
 }
