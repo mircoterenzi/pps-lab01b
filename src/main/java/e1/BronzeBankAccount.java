3dd7e9c170bf66918b8c346c323239793ca1843c
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-03 13:35:59 +0100

    add fee to BronzeBankAccount

--- src/main/java/e1/BronzeBankAccount.java
+++ src/main/java/e1/BronzeBankAccount.java
@@ -2,7 +2,17 @@ package e1;
 
 public class BronzeBankAccount extends BankAccountDecorator {
 
+    public static final int FEE = 1;
+
     public BronzeBankAccount(BankAccount account) {
         super(account);
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        if (this.getBalance() < amount){
+            throw new IllegalStateException();
+        }
+        super.account.withdraw(amount + FEE);
     }
 }
--- src/test/java/e1/BronzeBankAccountTest.java
+++ src/test/java/e1/BronzeBankAccountTest.java
@@ -1,10 +1,20 @@
 package e1;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BronzeBankAccountTest extends BankAccountTest {
     @BeforeEach
     void init(){
         super.account = super.factory.getBronzeBankAccount();
+    }
+
+    @Test
+    public void testWithdrawalFee() {
+        this.account.deposit(INITIAL_BALANCE);
+        this.account.withdraw(WITHDRAW_AMOUNT);
+        assertEquals(INITIAL_BALANCE - WITHDRAW_AMOUNT - 1, this.account.getBalance());
     }
 }
