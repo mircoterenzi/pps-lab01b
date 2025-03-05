65496f4fa71ef70dadc0424d7f0dc01348ffaaa0
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-05 15:10:45 +0100

    refactor test by adding a setUp method

--- src/test/java/e2/KnightMovementTest.java
+++ src/test/java/e2/KnightMovementTest.java
@@ -1,21 +1,27 @@
 package e2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class KnightMovementTest {
+
     public static final Pair<Integer, Integer> POSITION = new Pair<>(0, 0);
+    private MoveValidator validator;
+
+    @BeforeEach
+    public void setUp() {
+        this.validator = new KnightMoveValidator();
+    }
 
     @Test
     public void testIllegalMovement() {
-        MoveValidator validator = new KnightMoveValidator();
         assertFalse(validator.test(POSITION, new Pair<>(POSITION.getX() + 1, POSITION.getY() + 1)));
     }
 
     @Test
     public void testLegalMovement() {
-        MoveValidator validator = new KnightMoveValidator();
         assertTrue(validator.test(POSITION, new Pair<>(POSITION.getX() + 2, POSITION.getY() + 1)));
     }
 }
--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -10,34 +10,34 @@ public class LogicTest {
     public static final Pair<Integer, Integer> KINGHT_POSITION = new Pair<>(0, 0);
     private Logics logics;
 
+    @BeforeEach
+    public void setUp() {
+        this.logics = new LogicsImpl(SIZE, PAWN_POSITION, KINGHT_POSITION);
+    }
+
     @Test
     public void testMoveOutsideBoard() {
-        this.logics = new LogicsImpl(SIZE);
         assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(SIZE, SIZE));
     }
 
     @Test
     public void testHasPawn() {
-        this.logics = new LogicsImpl(SIZE, PAWN_POSITION, KINGHT_POSITION);
         assertTrue(this.logics.hasPawn(PAWN_POSITION.getX(), PAWN_POSITION.getY()));
     }
 
     @Test
     public void testHasKnight() {
-        this.logics = new LogicsImpl(SIZE, PAWN_POSITION, KINGHT_POSITION);
         assertTrue(this.logics.hasKnight(KINGHT_POSITION.getX(), KINGHT_POSITION.getY()));
     }
 
     @Test
     public void testIllegalMove() {
-        this.logics = new LogicsImpl(SIZE, PAWN_POSITION, KINGHT_POSITION);
         this.logics.hit(KINGHT_POSITION.getX() + 1, KINGHT_POSITION.getY() + 1);
         assertTrue(this.logics.hasKnight(KINGHT_POSITION.getX(), KINGHT_POSITION.getY()));
     }
 
     @Test
     public void testEatPawn() {
-        this.logics = new LogicsImpl(SIZE, PAWN_POSITION, KINGHT_POSITION);
         assertTrue(this.logics.hit(PAWN_POSITION.getX(), PAWN_POSITION.getY()));
     }
 }
