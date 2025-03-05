258e5c977e6c0ebfa856c1e3325220b90d9cf6ad
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-05 14:02:44 +0100

    add test move outside the board

--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -1,15 +1,16 @@
 package e2;
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LogicTest {
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
-  }
+    public static final int SIZE = 2;
+    private Logics logics;
+
+    @Test
+    public void testMoveOutsideBoard() {
+        this.logics = new LogicsImpl(SIZE);
+        assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(SIZE, SIZE));
+    }
 }
