f72c2efb94ece5987fe04a48aca03a31a8d8c8c9
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-05 14:15:53 +0100

    add test move

--- src/main/java/e2/LogicsImpl.java
+++ src/main/java/e2/LogicsImpl.java
@@ -1,5 +1,6 @@
 package e2;
 
+import java.nio.charset.CodingErrorAction;
 import java.util.*;
 
 public class LogicsImpl implements Logics {
@@ -14,6 +15,12 @@ public class LogicsImpl implements Logics {
         this.pawn = this.randomEmptyPosition();
         this.knight = this.randomEmptyPosition();	
     }
+
+	public LogicsImpl(int size, Pair<Integer, Integer> pawn, Pair<Integer, Integer> knight) {
+		this.size = size;
+		this.pawn = pawn;
+		this.knight = knight;
+	}
     
 	private final Pair<Integer,Integer> randomEmptyPosition(){
     	Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -5,12 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LogicTest {
 
-    public static final int SIZE = 2;
+    public static final int SIZE = 4;
+    public static final Pair<Integer, Integer> PAWN_POSITION = new Pair<>(SIZE - 1, SIZE - 1);
+    public static final Pair<Integer, Integer> KINGHT_POSITION = new Pair<>(0, 0);
     private Logics logics;
 
     @Test
     public void testMoveOutsideBoard() {
         this.logics = new LogicsImpl(SIZE);
         assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(SIZE, SIZE));
+    }
+
+    @Test
+    public void testIllegalMove() {
+        this.logics = new LogicsImpl(SIZE, PAWN_POSITION, KINGHT_POSITION);
+        this.logics.hit(KINGHT_POSITION.getX() + 1, KINGHT_POSITION.getY() + 1);
+        assertTrue(this.logics.hasKnight(KINGHT_POSITION.getX(), KINGHT_POSITION.getY()));
     }
 }
