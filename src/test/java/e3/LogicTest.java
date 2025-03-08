c3a9de4e7853e51170a5be926f4f69df83940cb5
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-08 12:28:24 +0100

    add test to check initial state of Logics

--- src/test/java/e3/LogicTest.java
+++ src/test/java/e3/LogicTest.java
@@ -1,11 +1,34 @@
 package e3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class LogicTest {
 
-    @Test
-    public void test() {
+    public static final int SIZE = 5;
+    private Logics logics;
 
+    @BeforeEach
+    public void setUp() {
+        this.logics = new LogicsImpl(SIZE);
+    }
+
+    @Test
+    public void testInitiallyNotFlagged() {
+        boolean isAtLeastOneFlagged = false;
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                Pair<Integer, Integer> pos = new Pair<>(x, y);
+                isAtLeastOneFlagged = isAtLeastOneFlagged || this.logics.isFlagged(pos);
+            }
+        }
+        assertFalse(isAtLeastOneFlagged);
+    }
+
+    @Test
+    public void testInitiallyGameNotWon() {
+        assertFalse(this.logics.isGameCompleted());
     }
 }
