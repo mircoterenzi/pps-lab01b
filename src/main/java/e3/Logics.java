3dfb79d372d06720fed0cea9dc13f70214ebf5e1
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-08 12:27:00 +0100

    add needed methods to interface and impl class

--- src/main/java/e3/Logics.java
+++ src/main/java/e3/Logics.java
@@ -1,5 +1,48 @@
 package e3;
 
+import java.util.Optional;
+
 public interface Logics {
-    
+
+    /**
+     * Check the cell and, if it is not a mine, calculate the number of adjacent mines
+     * (also for adjacent cells if the cell has no mines nearby).
+     * @param pos The position of the cell to be checked.
+     * @return true if the cell is a mine, false otherwise.
+     */
+    boolean openCell(Pair<Integer, Integer> pos);
+
+    /**
+     * Tell if the game has been completed.
+     * @return true if the game is won, false otherwise.
+     */
+    boolean isGameCompleted();
+
+    /**
+     * Set flag value for a given cell.
+     * @param pos The position of the cell.
+     * @param value True if the cell has to be flagged, false otherwise.
+     */
+    void setFlag(Pair<Integer, Integer> pos, boolean value);
+
+    /**
+     * Check if the cell is flagged.
+     * @param pos The position of the cell.
+     * @return True if the cell is flagged, false otherwise.
+     */
+    boolean isFlagged(Pair<Integer, Integer> pos);
+
+    /**
+     * Check if the cell is a mine.
+     * @param pos The position of the cell.
+     * @return True if the cell is a mine, false otherwise.
+     */
+    boolean isMine(Pair<Integer, Integer> pos);
+
+    /**
+     * Get, if present, the counter of adjacent mines.
+     * @param pos The position of the cell.
+     * @return An optional containing the counter value if it is present, an empty value otherwise.
+     */
+    Optional<Integer> getCounter(Pair<Integer, Integer> pos);
 }
--- src/main/java/e3/LogicsImpl.java
+++ src/main/java/e3/LogicsImpl.java
@@ -1,8 +1,39 @@
 package e3;
 
+import java.util.Optional;
+
 public class LogicsImpl implements Logics {
 
     public LogicsImpl(int size) {
     }
 
+    @Override
+    public boolean openCell(Pair<Integer, Integer> pos) {
+        return false;
+    }
+
+    @Override
+    public boolean isGameCompleted() {
+        return false;
+    }
+
+    @Override
+    public void setFlag(Pair<Integer, Integer> pos, boolean value) {
+
+    }
+
+    @Override
+    public boolean isFlagged(Pair<Integer, Integer> pos) {
+        return false;
+    }
+
+    @Override
+    public boolean isMine(Pair<Integer, Integer> pos) {
+        return false;
+    }
+
+    @Override
+    public Optional<Integer> getCounter(Pair<Integer, Integer> pos) {
+        return Optional.empty();
+    }
 }
